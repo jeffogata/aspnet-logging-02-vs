beb634eab6bdfca49bff6c33dbd4a995ce6638f5
Jeff Ogata <jeffogata@outlook.com>
2016-01-30 09:31:05 -0600

    initial commit

---
+++ src/aspnet-logging-02-vs/MyClass.cs
@@ -0,0 +1,37 @@
+﻿namespace aspnet_logging_02_vs
+{
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Logging.Internal;
+
+    public class MyClass
+    {
+        private readonly ILogger<MyClass> _logger;
+
+        public MyClass(ILogger<MyClass> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoSomething(int input)
+        {
+            var values = new FormattedLogValues("Starting to do something with input: {0}", input);
+
+            _logger.LogDebug(values);
+
+            if (input >= -1 && input <= 1)
+            {
+                _logger.LogInformation("Input of {0} is within the optimal range.", input);
+            }
+            else if (input > 10)
+            {
+                _logger.LogWarning("Input of {0} is greater than the typical range.", input);
+            }
+            else if (input < -10)
+            {
+                _logger.LogError("Input of {0} is less than the typical range.", input);
+            }
+
+            _logger.LogDebug("Finished doing something with input: {0}", input);
+        }
+    }
+}
---
+++ src/aspnet-logging-02-vs/Startup.cs
@@ -0,0 +1,49 @@
+﻿namespace aspnet_logging_02_vs
+{
+    using System.IO;
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.PlatformAbstractions;
+    using Serilog;
+    using Serilog.Events;
+    using Serilog.Sinks.RollingFile;
+
+    public class Startup
+    {
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.RollingFile(
+                    Path.Combine(appEnv.ApplicationBasePath, "log-{Date}.txt"), 
+                    LogEventLevel.Debug)
+                .WriteTo.LiterateConsole(LogEventLevel.Information)
+                .CreateLogger();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<MyClass>();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddSerilog();
+
+            app.UseIISPlatformHandler();
+
+            app.Run(async context =>
+            {
+                var myClass = context.RequestServices.GetService<MyClass>();
+
+                myClass.DoSomething(1);
+                myClass.DoSomething(20);
+                myClass.DoSomething(-20);
+
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
